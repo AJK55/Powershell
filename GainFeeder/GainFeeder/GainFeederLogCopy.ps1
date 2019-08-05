cc12375528e16e05cf5cf5c686559b7496d8d0c8
AJK55 <40635902+AJK55@users.noreply.github.com>
2019-08-05 16:45:42 -0400

    Add files via upload

---
+++ GainFeeder/GainFeeder/GainFeederLogCopy.ps1
@@ -0,0 +1,12 @@
+﻿$file = "GainFeeder.log"
+$sources = Get-Content $PSSCriptRoot\serverList.txt
+
+foreach ($source in $sources) {
+Test-Path $source\$file
+}
+
+
+foreach ($source in $sources) {
+$server = $source.TrimStart("\PKH-PPE").TrimEnd("Logs\MetaTrader4Server")
+Get-ChildItem -Path $source\$file | sort LastWriteTime | select -last 1 | Copy-Item -Destination "$PSSCriptRoot\Logs\ $server $(get-date -uformat %d%m%Y%H%M%S).txt"
+}
---
+++ GainFeeder/GainFeeder/GainFeederTail.ps1
@@ -0,0 +1,13 @@
+﻿$file = "GainFeeder.log"
+$sources = Get-Content $PSScriptRoot\serverList.txt
+
+Start-Transcript "$PSScriptRoot\Logs\ Logs $(get-date -uformat %d%m%Y%H%M%S).txt" -Append
+foreach ($source in $sources) {
+    if(Test-Path $source\$file){
+        Write-Host ("`n $source\$file") 
+        Get-Content $source\$file -Tail 10
+    }else {
+        Write-Host ("`n File DOES NOT exist `n","$source\$file") -ForegroundColor Red
+    }
+}
+Stop-Transcript
