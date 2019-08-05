c5c87f32d6a77efe67267880d8a7fda9d79682be
AJK55 <40635902+AJK55@users.noreply.github.com>
2019-08-05 16:45:08 -0400

    Add files via upload

---
+++ Selenium/Test1.ps1
@@ -0,0 +1,11 @@
+﻿# Install-Module Selenium
+# Need to install on initial install
+
+$Driver = Start-SeChrome
+Enter-SeUrl https://www.google.com -Driver $Driver
+$Driver.Navigate().GotoURL("https://www.forex.com/en-us/")
+$Driver.Navigate().GotoURL("https://www.google.com")
+$Driver.FindElementByCssSelector('#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input').SendKeys('Hello')
+#$Driver.FindElementByCssSelector('#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input').SendKeys(Keys.ENTER)
+#$Driver.FindElementByCssSelector('#tsf > div:nth-child(2) > div > div.UUbT9 > div.aajZCb > div.VlcLAe > center > input.gNO89b').Click()
+$Driver.Close()
